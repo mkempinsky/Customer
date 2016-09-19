c9094e1876a919259c2f9c5f5726fb8b56634176
Your Name <christiannaga1@gmail.com>
2016-09-19 13:56:10 -0700

    Added States

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -10,14 +10,24 @@
 
     function appConfig($urlRouterProvider, $stateProvider) {
 
-    	$urlRouterProvider.otherwise('/landing');
+    	$urlRouterProvider.otherwise('/restaurants');
 
     	$stateProvider
-    	.state('landing', {
-    		url: '/landing',  
-    		controller: 'LandingController as landing',
-    		templateUrl: 'js/landing/landing.html'
-    	})
+    	.state('restaurants', {
+            url: '/restaurants', 
+            abstract: true, 
+            template: '<div ui-view></div>'
+        })
+            .state('restaurants.list', {
+                url: '/list',
+                controller: 'RestaurantListController as restaurantList',
+                templateUrl: 'js/restaurants/list.html'
+            })
+            .state('restaurants.detail', {
+                url: '/detail?restaurantId',
+                controller: 'RestaurantDetailController as restaurantDetail',
+                templateUrl: 'js/restaurants/detail.html'
+            })
         .state('login', {
             url: '/login',  
             controller: 'LoginController as login',
@@ -28,11 +38,7 @@
             controller: 'OrdersController as orders',
             templateUrl: 'js/orders/orders.html'
         })
-        .state('restaurants', {
-            url: '/restaurants', 
-            controller: 'RestaurantsController as restaurants',
-            templateUrl: '/js/restaurants/restaurants.html'
-        });
+            
     }
 
 })();
