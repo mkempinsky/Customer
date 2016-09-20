ac228e471134fef46f527d1be51a04398f417c0f
reggie sibley <reggiewsibley@icloud.com>
2016-09-20 16:01:03 -0700

    Adding Menu Groups Items Remove Items

--- app/js/restaurant/restaurant.detail.controller.js
+++ app/js/restaurant/restaurant.detail.controller.js
@@ -11,23 +11,41 @@
     function RestaurantDetailController($stateParams, $state, restaurantFactory) {
         var vm = this;
         vm.title = 'restaurantDetailController';
-        vm.menu = [];
-
+        vm.menu = {};
+        vm.addToCart = addToCart;
         getMenu();
 
         ////////////////
 
         function getMenu() {
-            console.log($stateParams);
             restaurantFactory.getById(2).then(
             function(response) {
-                console.log(response); 
+                console.log(response);
                 vm.menu = response;
+                 vm.cart = {
+                    cost: function() {
+                        if(vm.cart.items.length) {
+                            return vm.cart.items.map(function(item) {
+                                return item.price;
+                            }).reduce(function(previousPrice, currentPrice) {
+                                return previousPrice + currentPrice;
+                            });
+                        } else {
+                            return 0;
+                        }
+                    },
+                    items: []
+                };
             },
             function(error){
                 console.log(error);
             }
             );
+        }
+
+
+        function addToCart(item) {
+            vm.cart.items.push(angular.copy(item));
         }
     }
 })();
