f533f701cce0f4329815b7ddcb1f90debd6f04d0
Michele Kempinsky <mkempinsky@icloud.com>
2016-09-25 23:41:25 -0700

    style

--- app/js/restaurant/restaurant.list.controller.js
+++ app/js/restaurant/restaurant.list.controller.js
@@ -5,17 +5,34 @@
 		.module('app')
 		.controller('RestaurantListController', RestaurantListController);
 
-	RestaurantListController.$inject = ['$stateParams', 'restaurantFactory'];
+	RestaurantListController.$inject = ['$stateParams', 'restaurantFactory', 'customerFactory'];
 
-	function RestaurantListController($stateParams, restaurantFactory){
+	function RestaurantListController($stateParams, restaurantFactory, customerFactory){
 		var vm = this;
 
 		vm.allRestaurants = [];
+		vm.customer = {};
 		vm.restaurantId = $stateParams.restaurantId;
 		vm.customerId = $stateParams.customerId;
-		getAllRestaurants();
 
-/*************************************************************************/
+		getAllRestaurants();
+		getCustomerInfo();
+
+        function getCustomerInfo() {
+        	if ($stateParams.customerId) {
+        		customerFactory.getById($stateParams.customerId).then(
+        			function(data) {
+        				vm.customer = data;
+                        console.log(vm.customer);
+        			}
+        		);
+        	}
+        	else
+        	{
+        		vm.customer = {};
+        	}
+        }
+
 		function getAllRestaurants(){
 			restaurantFactory.getAll().then(
 				function(data){
